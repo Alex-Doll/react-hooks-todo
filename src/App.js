6e50eaa51cfbe200f4cd27682cf3d5ae50ecd48c
rosco1010 <doll3098@ou.edu>
2019-01-01 12:11:08 -0600

    Add complete and remove buttons for todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
-    <div className='todo'>
+    <div
+      className='todo'
+      style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
+    >
       {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
     </div>
   );
 }
@@ -53,11 +60,29 @@ function App() {
     setTodos(newTodos);
   }
 
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <div className='App'>
       <div className='todo-list'>
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
